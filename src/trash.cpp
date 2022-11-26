114abae8e33f4bf72602a7dcf94f741730a3e4d2
probonopd <probonopd@users.noreply.github.com>
2022-11-26 12:31:10 +0100

    Delete items in Trash properly

--- src/trash.cpp
+++ src/trash.cpp
@@ -1,6 +1,7 @@
 #include "trash.h"
 #include "fileoperation.h"
 #include <QDir>
+#include <QFileInfo>
 #include <QDebug>
 #include <QFile>
 #include <QStandardPaths>
@@ -8,17 +9,28 @@
 #include "sound.h"
 
 void Fm::Trash::emptyTrash(){
+
+    // TODO: This deletes the files but does not manage the trash "database"
     QString path = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + "/Trash/files";
     QDir dir(path);
 
-    dir.setFilter(QDir::NoDotAndDotDot | QDir::Files);
-    for(const QString dirItem : dir.entryList())
-        dir.remove( dirItem );
+    dir.setFilter(QDir::NoDotAndDotDot | QDir::Files | QDir::System);
+    for(const QString dirItem : dir.entryList()) {
+        qDebug() << "Deleting:" << dir.path() + dirItem;
+        dir.remove(dirItem);
+    }
 
     dir.setFilter(QDir::NoDotAndDotDot | QDir::Dirs);
     for(const QString dirItem : dir.entryList())
     {
         QDir subDir(dir.absoluteFilePath(dirItem));
-        subDir.removeRecursively();
+        if(QFileInfo(subDir.path()).isSymbolicLink()) {
+            qDebug() << "Deleting:" << dir.path() + subDir.path();
+            dir.remove(subDir.path());
+        } else {
+            qDebug() << "Deleting recursively:" << dir.absoluteFilePath(dirItem);
+            subDir.removeRecursively();
+        }
     }
+
 }
